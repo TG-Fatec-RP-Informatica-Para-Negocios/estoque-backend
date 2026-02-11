87a58106e813d8c0cf2aa87cd77fb8b0071209ec
grabaloss <dgrabalos.lucas@gmail.com>
2026-02-11 18:06:15 -0300

    Primeiro commit: Configuração inicial do Spring Boot

---
+++ src/main/java/com/tg/estoque_saas/EstoqueSaasApplication.java
@@ -0,0 +1,13 @@
+package com.tg.estoque_saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueSaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tg/estoque_saas/EstoqueSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tg.estoque_saas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
